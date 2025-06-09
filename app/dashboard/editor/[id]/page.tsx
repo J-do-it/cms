7ee8822714d4b19027d661672db10b35e8c2cdf3
J-do-it <alex1016@naver.com>
2025-06-09 19:56:55 +0900

    기사 편집 페이지에서 작성자 필드의 타입을 문자열로 명시하여 타입 안정성 향상

--- app/dashboard/editor/[id]/page.tsx
+++ app/dashboard/editor/[id]/page.tsx
@@ -247,7 +247,7 @@ const EditorPage = () => {
               type="text"
               id="author"
               name="author"
-              value={article.author || ''}
+              value={article.author as string || ''}
               onChange={handleAuthorChange}
               className="mt-1 block w-full px-4 py-3 bg-white text-black border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-lg"
               placeholder="작성자"
